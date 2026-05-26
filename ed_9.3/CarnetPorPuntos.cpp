7cd3687193700647d1f92f37b589edeed7686b1a
Washing-Mr8 <washing.22ml@gmail.com>
2026-05-26 20:24:33 +0200

    Carnet por puntos

---
+++ ed_9.3/CarnetPorPuntos.cpp
@@ -0,0 +1,77 @@
+﻿
+// Autor/a: Washington Morocho Lema
+// email: wmorocho@ucm.es
+// Nombre del problema: Carnet por puntos
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+#include "CarnetPorPuntos.h"
+
+
+bool resuelveCaso() {
+	std::string op, dni;
+	int punt;
+	std::cin >> op;
+	if (!std::cin)
+		return false;
+
+	CarnetPuntos C;
+
+	while (op != "FIN") {
+		try {
+			if (op == "nuevo") {
+				cin >> dni;
+				C.nuevo(dni);
+			}
+			else if (op == "quitar") {
+				cin >> dni >> punt;
+				C.quitar(dni, punt);
+			}
+			else if (op == "consultar") {
+				cin >> dni;
+				int puntos = C.consultar(dni);
+
+				cout << "Puntos de " << dni << ": " << puntos << "\n";
+
+			}
+			else if (op == "cuantos_con_puntos") {
+				cin >> punt;
+				int cuantos = C.cuantos_con_puntos(punt);
+
+				cout << "Con " << punt << " puntos hay " << cuantos << "\n";
+
+			}
+			
+		}
+		catch (std::domain_error e) {
+			std::cout << "ERROR: " << e.what() << "\n";
+		}
+		std::cin >> op;
+	}
+	std::cout << "---\n";
+	return true;
+}
+
+	
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("9_3.in");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+	/*std::ofstream out("datos.out");
+	auto coutbuf = std::cout.rdbuf(out.rdbuf());*/
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	//std::cout.rdbuf(coutbuf);
+#endif
+	return 0;
+}
---
+++ ed_9.3/CarnetPorPuntos.h
@@ -0,0 +1,81 @@
+﻿// Autor/a: Washington Morocho Lema
+// email: wmorocho@ucm.es
+// Nombre del problema: Carnet por puntos
+
+
+#ifndef CARNET
+#define CARNET
+
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <vector>
+
+
+using namespace std;
+
+const int PUNTOS_INICIALES = 15;
+
+class CarnetPuntos {
+private:
+	// ATRIBUTOS
+	unordered_map<string, int> conductores;
+	vector<int> puntosConductores;
+
+public:
+
+	// MÉTODOS
+	//CONSTRUCTORA PARA INICIALIZAR EL ARRAY (16 porque contamos el 0 tambien)
+	CarnetPuntos() : puntosConductores(16) {};
+
+	//Coste: O(1) o O(N) en funcion del contenido de la tabla hash. El promedio suele ser constante por el insert
+	void nuevo(const string& dni) {
+		if (conductores.count(dni) == 1) {
+			throw std::domain_error("Conductor duplicado");
+		}
+
+		conductores.insert({dni,PUNTOS_INICIALES});
+		puntosConductores[PUNTOS_INICIALES]++;
+	}
+
+	//Coste: O(1) La operacion erase es constante con unordered_map
+	void quitar(const string& dni, int puntos) {
+		if (conductores.count(dni) == 0) {
+			throw std::domain_error("Conductor inexistente");
+		}
+
+		puntosConductores[conductores[dni]]--;
+		int puntosActualizados = conductores[dni] - puntos;
+
+		if (puntosActualizados < 0) {
+			puntosActualizados = 0;
+		}
+
+		conductores[dni] = puntosActualizados;
+		puntosConductores[puntosActualizados]++;
+	}
+
+
+	//Coste: O(1) consultar con count en un unordered_map es constante 
+	int consultar(const string& dni) const{
+		if (conductores.count(dni) == 0) {
+			throw std::domain_error("Conductor inexistente");
+		}
+		return conductores.at(dni);
+	}
+
+	//Coste: O(1) consultar con count en un unordered_map es constante. Usamos un unordered_map con un tamaño maximo de 15, en el que guardamos los puntos como clave
+	// y almacenamos la cantidad de conductores que tienen esos puntos como valor;
+
+	int cuantos_con_puntos(int puntos) const {
+		if (puntos < 0 || puntos > 15) {
+			throw std::domain_error("Puntos no validos");
+		}
+
+		return puntosConductores[puntos];
+	}
+
+};
+#endif
